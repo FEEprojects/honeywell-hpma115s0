d8e2e31471000694d746cc717b822841b27abb16
Philip Basford <p.j.basford@soton.ac.uk>
2019-01-28 17:00:00 +0000

    Add other files required for packaging

---
+++ README.md
@@ -0,0 +1,6 @@
+# Honeywell HPMA115S0 Particulate Sensor Python interface
+A basic python interface for interacting with the Honeywell HPMA115S0 Particulate Matter sensor.  This code has been tested with the following devices: HPMA115S0.
+ 
+This wrapper is based on: https://github.com/FEEprojects/plantower
+
+This wrapper has been developped as part of on-going work on the use of low-cost PM sensors to monitor air quality in urban areas.
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="honeywell_hpma115s0",
+    version="0.0.1",
+    author="Florentin Bulot",
+    author_email="f.bulot@soton.ac.uk",
+    description="An interface for honeywell HPMA115S0 particulate matter sensors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FEEprojects/honeywell-hpma115s0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+     python_requires='>=3.3, <4',
+     install_requires=['pyserial']
+)
